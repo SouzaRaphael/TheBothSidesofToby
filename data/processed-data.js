35ad9cfea8e9d5c15919f3a8a96de736f002ad00
SouzaRaphael <dainol2012@gmail.com>
2023-12-07 08:32:41 -0300

    Demo complete

--- data/processed-data.js
+++ data/processed-data.js
@@ -6,6 +6,12 @@ var characters =
     "name":""
   },
 
+  "professor":{
+    "poses":{
+    },
+    "name":"Professor"
+  },
+
   "narrador":{
     "poses":{
     },
@@ -113,17 +119,27 @@ var characters =
 
 	"saladeaula" : {
 		"name":"Sala de Aula",
-		"image":"img/Cozinha2.jpg"
+		"image":"img/SaladeAula.jpg"
 	},
 
 	"festa" : {
 		"name":"A festa",
-		"image":"img/Cozinha2.jpg"
+		"image":"img/Festa.jpg"
 	},
 
 	"quartohospital" : {
 		"name":"O hospital",
-		"image":"img/Cozinha2.jpg"
+		"image":"img/QuartoHospital.jpg"
+	},
+
+	"saladotecnico" : {
+		"name":"Sala de Robótica",
+		"image":"img/SaladoTecnico.jpg"
+	},
+
+	"..." : {
+		"name":"???",
+		"image":""
 	}
 }; 
  var story = 
@@ -131,25 +147,25 @@ var characters =
 	{
 		"title": "Start",
 		"tags": "",
-		"body": "<<place quarto>>\n**TRIIIM** **TRIIIM** **TRIIIM**\n{{toby}}!!!\n{{toby}}(Nossa, já está de manhã)\n{{toby,doubt}}(Que sono...)\n{{narrador}}Toby então se levanta e vai até o bannheiro. Escova seus dentes e toma um banho rápido, a fim de conseguir se arrumar a tempo de ir à escola.\n[[{{narrador}}Voltando para o quarto...|camisa?]]"
+		"body": "<<place ...>>\n{{narrador}}...\n{{narrador}}...hm?\n{{narrador}}Ah!\n{{narrador}}Olá, seja bem vindo a demo da nossa história interativa chamada: 'The Both Sides of Toby'! (ou 'os dois lados de Toby').\n{{narrador}}Nela, você acompanhará a narrativa do Toby.\n{{toby}}...\n{{toby,worried}}Onde eu tô?\n{{narrador}}Ele é um garoto que sofre de Transtorno Bipolar.\n{{narrador}}E você tomará decisões que podem (e <b>VÃO</b>) mudar a história dele.\n{{narrador}}Sem mais enrolação, vamos começar!\n<<place quarto>>\n...\n**TRIIIM** **TRIIIM** **TRIIIM**\n{{toby}}!!!\n{{toby}}(Nossa, já está de manhã)\n{{toby,doubt}}(Que sono...)\n{{narrador}}Toby então se levanta e vai até o bannheiro. Escova seus dentes e toma um banho rápido, a fim de conseguir se arrumar a tempo de ir à escola.\n[[{{narrador}}Voltando para o quarto...|camisa?]]"
 	},
 	
 	{
 		"title": "camisa?",
 		"tags": "",
-		"body": "{{toby}}(Qual camiseta eu vou usar hoje?)\n[[A de sempre|cafedamanha]]\n[[Uma preta com estampa de caveira|punk?]]\n[[Uma branca com estampa de anime|nerd?]]"
+		"body": "{{toby}}(Qual camiseta eu vou usar hoje?)\n[[A de sempre.|cafedamanha]]\n[[Uma preta com estampa de caveira.|punk?]]\n[[Uma branca com estampa de anime.|nerd?]]"
 	},
 
 	{
 		"title": "punk?",
 		"tags": "",
-		"body": "{{toby,worried}}(Será que não me achariam punk demais?)\n[[O corpo é meu, eu decido o que vou usar nele|cafedamanha]]\n[[É, melhor não|camisa?]]"
+		"body": "{{toby,worried}}(Será que não me achariam punk demais?)\n[[O corpo é meu, eu decido o que vou usar nele.|cafedamanha]]\n[[É, melhor não.|camisa?]]"
 	},
 	
 	{
 		"title": "nerd?",
 		"tags": "",
-		"body": "{{toby,worried}}(Será que não me achariam nerd demais?)\n[[Bobagem, é algo que eu gosto, por que achariam isso?|acaocamiseta]]\n[[É, melhor não|camisa?]]"
+		"body": "{{toby,worried}}(Será que não me achariam nerd demais?)\n[[Bobagem, é algo que eu gosto, por que achariam isso?|acaocamiseta]]\n[[É, melhor não.|camisa?]]"
 	},
 
 	{
@@ -203,7 +219,7 @@ var characters =
 	{
 		"title": "consequenciaroupa",
 		"tags": "",
-		"body": "{{raquel,confused}}Nossa, Toby, que roupa é essa?\n<<number depressivo + 1>>\n{{toby,angry}}Oi pra você também, Raquel\n{{bruno,worried}}Que isso, Raquel, deixa o garoto se vestir.\n{{raquel,angry}}Ah, cala a boca Bruno\n[[{{toby,happy}}Eai, Bruno|escolhaespelho]]"
+		"body": "{{raquel,confused}}Nossa, Toby, que roupa é essa?\n<<number depressivo + 1>>\n{{toby,angry}}Oi pra você também, Raquel.\n{{bruno,worried}}Que isso, Raquel, deixa o garoto se vestir.\n{{raquel,angry}}Ah, cala a boca Bruno.\n[[{{toby,happy}}Eai, Bruno|escolhaespelho]]"
 	},
 
 	{
@@ -212,96 +228,57 @@ var characters =
 		"body": "{{raquel,happy}}Oi, Toby!\n{{bruno,happy}}Eai, Toby!\n[[{{toby,happy}}Oi gente!|escolhaespelho]]"
 	},
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	{
-		"title": "awkward_pause",
+		"title": "escolhaespelho",
 		"tags": "",
-		"body": "{{bobby}} ...Okay.\n//This is awkward. Why did you say no?\n...\n{{bobby}} ...\n[[...]]\n[[...So, about that secret ending?|secret]] <<random 50>>\n{{bobby}} Can we...\n[[Yeah fine, let's start.|explanation]]\n[[I said no.|awkward_pause]]"
+		"body": "{{raquel,confused}}Tem alguma coisa no seu rosto, Toby.\n{{toby,worried}}O que eu faço?\n[[Tenho um espelho na mochila, vou pegá-lo e ver o que tem no meu rosto.|pegouespelho]]\n[[Deve ser só uma das brincadeiras de mal gosto de Raquel, vou ignorar.|semespelho]]"
 	},
+	
 	{
-		"title": "explanation",
+		"title": "pegouespelho",
 		"tags": "",
-		"body": "{{bobby}} Well there's not much to say honestly.\n{{bobby}} The documentation in README.md should give you most of the information you need.\n{{bobby}} Have you read it?\n[[Yeah it's very well written.|you_lier]]\n[[Not yet.|go_read_it]]\n[[Yes and it sucks. I hated it.|i_know_right]]",
-		"position": {
-			"x": 615,
-			"y": 540
-		},
-		"colorID": 0
+		"body": "<<string espelho pegou>>\n{{narrador}}Toby retira o espelho da mochila e se olha nele para tirar a remela de seu olho.\n{{toby,happy}}Obrigado pelo aviso.\n[[{{raquel}}Bom, vamos entrar então?|spinoff]]"
 	},
+	
 	{
-		"title": "i_know_right",
+		"title": "semespelho",
 		"tags": "",
-		"body": "{{bobby,happy}} Hahaha I don't care, I'm just a character in this story.\n//Well, I care. But yeah, the doc sucks for now. Sorry.\n[[{{bobby}} Anyway...|thanks_bobby]]",
-		"position": {
-			"x": 856,
-			"y": 887
-		},
-		"colorID": 0
+		"body": "{{bruno,worried}}Toby, realmente te-\n[[{{toby,angry}}Ah para, Bruno! Eu conheço vocês. Vamos entrar, tá bom?|spinoff]]"
 	},
+	
 	{
-		"title": "you_lier",
+		"title": "spinoff",
 		"tags": "",
-		"body": "<<string liedToBobby true>>\n{{bobby}} You don't have to lie to me...\n{{bobby,happy}} I know it sucks, that's why I'm here to provide at least a working example!\n[[{{bobby}} Anyway...|thanks_bobby]]",
-		"position": {
-			"x": 374,
-			"y": 890
-		},
-		"colorID": 0
+		"body": "<<place corredor>>\n{{narrador}}Eles começam a andar enquanto raquel diz:\n{{raquel,happy}}Sabiam que vão lançar um spin-off do meu jogo favorito?!\n{{toby,happy}}Sério? Pensei que os criadores não fossem fazer mais nada.\n{{raquel}}Não são os mesmos criadores, nem a mesma história, mas vai ter a mesma pegada.\n{{bruno,confused}}Então não é um spin-off, Raquel.\n{{raquel,angry}}Ah, mas vocês entenderam.\n<<place saladotecnico>>\n{{narrador}}Eles entram em uma sala cheia de circuitos e peças.\n{{narrador}}Toby começa a programar em seu notebook, enquanto Raquel está soldando uma peça na outra e Bruno montando chips.\n{{bruno,worried}}Estou preocupado. Estamos entrando em Agosto e nosso projeto não está saindo como o esperado...\n{{raquel}}Como está a programação, Toby?\n{{toby,doubt}}Ainda não consegui resolver este maldito erro!\n{{bruno,worried}}Também, com o tanto de trabalhos que jogam em cima da gente, não da tempo de pensar no nosso projeto.\n{{raquel,angry}}Pare de reclamar tanto, Bruno!\n{{bruno,angry}}Temos 5 trabalhos pendentes fora o nosso projeto, Raquel, 5!\n{{raquel,confused}}Eu sei, mas o quanto antes você terminar a sua parte, mais tempo teremos para fazê-los.\n{{raquel,confused}}Agora anda logo que a aula acaba em 10 minutos.\n{{toby,worried}}10 minutos e eu ainda não consegui resolver o erro no código...\n{{toby,worried}}O que devo fazer?\n[[Preciso resolver este problema, não temos mais tempo para gastar!|prova]]\n[[Vou garantir que meus arquivos estejam salvos e seguros. Resolvo isso em casa.|prova]]"
 	},
+	
 	{
-		"title": "go_read_it",
+		"title": "prova",
 		"tags": "",
-		"body": "{{bobby,happy}} Then do it now! It's a bit long but there are important things to know.\n{{bobby}} It should give you some advice on how to get started with your own game.\n{{bobby}} I'll just wait here.\n{{bobby}} ...\n[[{{bobby,happy}} Oh, you must be done reading since you're back here!|thanks_bobby]]",
-		"position": {
-			"x": 611,
-			"y": 887
-		},
-		"colorID": 0
+		"body": "<<place saladeaula>>\n{{narrador}}Após a aula do técnico finalizar, Toby vai até a sala onde o professor de química está e senta em seu lugar, no aguardo para o início da prova.\n{{narrador}}5 minutos depois...\n{{professor}}Todos sentem em seus lugares, começarei a distribuir as provas a partir de agora!\n{{narrador}}O professor então, vai até o lugar onde Toby está sentado e o entrega a prova.\n{{narrador}}Olhando fixamente em seus olhos, o professor diz:\n[[{{professor}}Olha só essas remelas. Alguém aqui decidiu estudar para a prova só de última hora, não é mesmo?|risos]] <<string espelho != pegou>> \n [[{{professor}}Boa prova.|conclusao]] <<string espelho == pegou>>"
 	},
+	
 	{
-		"title": "thanks_bobby",
+		"title": "risos",
 		"tags": "",
-		"body": "{{bobby}} You can open this story file in your dialogue editor now.\n{{bobby,happy}} And then you should be ready to start working on your own game!\n{{alice}} Who's working on a game?\n{{bobby,happy}} Oh hi, Alice!\n{{alice}} Who are you talking to, Bobby?\n[[{{bobby}} I have no idea, but they're cool.]] <<string liedToBobby != true>>\n[[{{bobby}} I have no idea, but they're cool. Well they lied to me once but it's okay.]] <<string liedToBobby == true>>\n{{alice,happy}} Well good luck, stranger!\n[[{{bobby,happy}} See you soon!|end]]",
-		"position": {
-			"x": 606,
-			"y": 1272
-		},
-		"colorID": 0
+		"body": "[[{{narrador}}Toby ouve risos da sala inteira.|conclusao]]"
 	},
+	
 	{
-		"title": "secret",
+		"title": "conclusao",
 		"tags": "",
-		"body": "[[{{bobby,happy}} I'm glad you found it!|end]]",
-		"position": {
-			"x": 1210,
-			"y": 1279
-		},
-		"colorID": 0
+		"body": "<<place rua>>\n{{narrador}}Um dia muito estressante de aula depois...\n{{bruno,happy}}O que acharam da prova de química?\n{{raquel,confused}}Foi um saco.\n{{raquel,happy}}Mas eu gostei da parte das substâncias.\n{{toby}}Bom, é aqui que nos despedimos.\n{{raquel,happy}}Até mais, Toby!\n[[{{bruno,happy}}Falou, Toby.|final]] <<string espelho == pegou>> \n[[{{bruno,worried}}Não se preocupe com a recuperação, você é inteligente e se sairá bem!|agdcmento]] <<string espelho != pegou>>"
+	},
+	
+	{
+		"title": "agdcmento",
+		"tags": "",
+		"body": "[[{{toby}}Agradeço.|final]]"
+	},
+	
+	{
+		"title": "final",
+		"tags": "",
+		"body": "<<place quarto>>\n{{narrador}}Chegando em casa, Toby vai direto ao seu quarto. Joga sua mochila na mesa e capota na cama.\n[[{{narrador}}Fim da Demo.|end]]"
 	}
 ]; 
